6b9412baa09ac2aad2e0594ecbd0813e336fc506
BlacK201 <black201@foxmail.com>
2022-03-22 19:51:42 +0800

    增加自动接受对局功能 可以在设置页开启或关闭

--- main/lol_new.js
+++ main/lol_new.js
@@ -74,6 +74,7 @@ class LoL {
             isRuneOn: true,
             isItemBuildOn: true,
             isSpellOn: true,
+            isAutoAcceptOn: false,
             spellLocation: "d",
             isOverlayOn: false,
             region: null,
@@ -129,6 +130,10 @@ class LoL {
             this.app.window.getLocalStorage("autoitem")
                 .then((result) => {
                     this.config.isItemBuildOn = !(result === "false" || result === false);
+                })
+            this.app.window.getLocalStorage("autoaccept")
+                .then((result) => {
+                    this.config.isAutoAcceptOn = !(result === "false" || result === false);
                 })
             this.app.window.getLocalStorage("isSpell")
                 .then((result) => {
@@ -261,6 +266,15 @@ class LoL {
                             this.champSelectionSession(data);
                             break;
 
+                        case "/lol-matchmaking/v1/ready-check":
+                            console.log(data)
+                            if (data.state === "InProgress") {
+                                if (this.config.isAutoAcceptOn){
+                                    this.acceptMatch();
+                                }
+                            }
+                            break;
+
                         case lolConstants.LOL_PRESHUTDOWN_BEGIN:
                             this.ws.close();
                             break;
@@ -348,6 +362,10 @@ class LoL {
         } catch(e) {
 
         }
+    }
+
+    async acceptMatch() {
+        await this.callAPI("POST", "lol", "/lol-lobby-team-builder/v1/ready-check/accept").catch((_) => {console.log(_)});
     }
 
     checkProcess() {
@@ -1464,6 +1482,10 @@ class LoL {
 
         ipcMain.on("autoitem", (event, arg) => {
             this.config.isItemBuildOn = arg;
+        });
+
+        ipcMain.on("autoaccept", (event, arg) => {
+            this.config.isAutoAcceptOn = arg;
         });
 
         ipcMain.on("spell", (event, arg) => {
